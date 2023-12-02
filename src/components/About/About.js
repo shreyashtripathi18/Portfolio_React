3544857287a7584b3211d4fc797ec93cbaf7c19f
Shreyash Tripathi <76698429+shreyashtripathi18@users.noreply.github.com>
2023-12-03 01:13:45 +0530

    about-changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 import './index.scss'
 import { useEffect, useState } from 'react'
+import { faAngular, faCss3, faGitAlt, faHtml5, faJs, faReact } from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
+
 
 const About = () => {
   const[letterClass, setLetterClass] = useState('text-animate')
@@ -13,6 +17,7 @@ const About = () => {
 
 
   return (
+    <>
     <div className=" container about-page">
       <div className="text-zone">
         <h1>
@@ -35,8 +40,33 @@ const About = () => {
           If I need to define myself in one sentence that would be a family
           person, a sports fanatic, photography enthusiast, and tech-obsessed!!!
         </p>
+      
+      </div>
+      <div className="stage-cube-cont">
+        <div className="cubespinner">
+          <div className="face1">
+            <FontAwesomeIcon icon={faAngular} color='#dd0031'/>
+          </div>
+          <div className="face2">
+            <FontAwesomeIcon icon={faHtml5} color='#F06529'/>
+          </div>
+          <div className="face3">
+            <FontAwesomeIcon icon={faCss3} color='#28A4D9'/>
+          </div>
+          <div className="face4">
+            <FontAwesomeIcon icon={faReact} color='#5ED4F4'/>
+          </div>
+          <div className="face5">
+            <FontAwesomeIcon icon={faJs} color='#EFD81D'/>
+          </div>
+          <div className="face6">
+            <FontAwesomeIcon icon={faGitAlt} color='#EC4D28'/>
+          </div>
+        </div>
       </div>
     </div>
+    <Loader type="pacman" />
+    </>
   )
 }
 
