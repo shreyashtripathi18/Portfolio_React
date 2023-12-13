3d00d5b248bc2ecea47f46a02d0b64e3dd71530f
Shreyash Tripathi <76698429+shreyashtripathi18@users.noreply.github.com>
2023-12-14 00:37:58 +0530

    Final

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,18 +1,43 @@
 import Loader from 'react-loaders'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
-import { useEffect,useState } from 'react'
+import { useEffect,useRef,useState } from 'react'
 import { Form } from 'react-router-dom'
+import emailjs  from '@emailjs/browser'
+import {MapContainer, TileLayer, Popup, Marker} from 'react-leaflet'
 
 
 const Contact = () => {
     const[letterClass, setLetterClass] = useState('text-animate')
+    const refForm = useRef()
   
     useEffect(() => {
       const timeoutId = setTimeout(() => {
         setLetterClass('text-animate-hover');
       }, 4000);
       }, []);
+
+
+      const sendEmail = (e) =>{
+        e.preventDefault()
+
+        emailjs
+            .sendForm(
+                'service_jsal7ob',
+                'template_sfd1uea',
+                refForm.current,
+                'bruzmZPQGn5r1Z97c'
+            )
+            .then(
+                () => {
+                    alert("Email sent Successfully");
+                    window.location.reload(false)
+                },
+                () => {
+                    alert('Failed to send message , Try again')
+                }
+            )
+      }
 
     return(
         <>
@@ -31,7 +56,7 @@ const Contact = () => {
                         don't hesitate to contact me using below form either.
                     </p>
                     <div className="contact-form">
-                        <form ref={Form} >
+                        <form ref={Form} onSubmit={sendEmail} >
                             <ul>
                                 <li className="half">
                                     <input type="text" name="name" placeholder="name" required />
@@ -51,6 +76,23 @@ const Contact = () => {
                             </ul>
                         </form>
                     </div>
+                </div>
+                <div className="info-map">
+                    Shreyash Tripathi
+                    <br />
+                    India
+                    <br />
+                    Orai, Jalaun<br />
+                    Uttar Pradesh <br />
+                    <span>shreyasht2002@gmail.com</span>
+                </div>
+                <div className="map-wrap">
+                    <MapContainer center={[25.981634230182483, 79.45058911227402]} zoom={13}>
+                        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                        <Marker position={[25.981634230182483, 79.45058911227402]}>
+                            <Popup>Shreyash lives here, come over for a cup of coffee :D</Popup>
+                        </Marker>
+                    </MapContainer>
                 </div>
             </div>      
             <Loader type="pacman"/>
