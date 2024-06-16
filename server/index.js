6e179d0cc790d6037d9b19572c6665fbb4bb1873
Shreyash Tripathi <76698429+shreyashtripathi18@users.noreply.github.com>
2024-06-16 14:41:38 +0530

    server setup

---
+++ server/index.js
@@ -0,0 +1,18 @@
+// index.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, '../build')));
+
+// Handle any requests that don't match the ones above
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
